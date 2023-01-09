189132d241955de3204931482512d5077e7e817d
Balaji Sundararaman <32245361+bsun210@users.noreply.github.com>
2023-01-08 16:42:12 -0800

    Update gc_cover_plot.py

--- gc_cover_plot.py
+++ gc_cover_plot.py
@@ -1,5 +1,5 @@
 #Balaji Sundararaman
-#01-15-2022
+#03-28-2022
 #usage python3 <perCENR_coverage_from_picard.tsv> <file_name_prefix>
 
 import matplotlib
@@ -21,30 +21,36 @@ dfx = pd.read_csv(sys.argv[1],header=0,index_col=4,sep="\t")
 bins = list(b/100.0 for b in range(0,101,1))
 #print(len(bins))
 gc_hist = np.histogram(dfx['%gc'], bins=bins)
+#print(len(gc_hist[0]))
+#print(len(gc_hist[1]))
+
 
 gc_cov_weight = np.histogram(dfx['%gc'], weights=dfx['normalized_coverage'], bins=bins)
 normCov_gc = np.nan_to_num(gc_cov_weight[0]/gc_hist[0],copy=False)
 for i in range(len(bins[1:])):
     print(bins[i], normCov_gc[i], gc_hist[0][i])
 
-fig1, ax1 = plt.subplots()
 
-#ax1.set_title(sys.argv[2], fontsize=14)
-ax1.set_xlabel('GC Content', fontsize=16) # fontweight='semibold', fontsize=16, family='Arial', labelpad=15)
+plt.figure(figsize=(5,4))
+ax1=plt.axes([0.15,0.15,0.67,0.75])
+    
+ax1.set_title(sys.argv[2], fontsize=14)
+ax1.set_xlabel('GC Content', fontsize=14) # fontweight='semibold', fontsize=16, family='Arial', labelpad=15)
 new_y = np.ma.masked_where(normCov_gc==0, normCov_gc)
 ax1.scatter(bins[1:], new_y, s=3, color='black')
-ax1.set_ylabel('Normalized Coverage', fontsize=16) # fontweight='semibold', fontsize=16, family='Arial', labelpad=15)
+ax1.set_ylabel('Normalized Coverage', fontsize=14) # fontweight='semibold', fontsize=16, family='Arial', labelpad=15)
 ax1.axhline(y=1, color='black', alpha = 0.7, lw = 1, linestyle='-.')
-ax1.tick_params(axis='both', which='major', labelsize=13, length=6, width=1)
+ax1.tick_params(axis='both', which='major', labelsize=12, length=6, width=1)
+ax1.set_ylim(-0.05, 2.5)
 
 ax2 = ax1.twinx()
 ax2.hist(dfx['%gc'], bins=bins, color='black', alpha=0.35)
 ax2.set_ylim(0, 4000)
 ax2.set_yticks([yy for yy in range(0,4001,1000)])
 ax2.set_yticklabels(['0','1000','2000','3000','4000'])
-ax2.set_ylabel('Target GC Histogram', fontsize=16) #fontweight='bold', fontsize=16, family='Sans', labelpad=10)
-ax2.tick_params(axis='both', which='major', labelsize=13, length=6, width=1)
+ax2.set_ylabel('Target GC Histogram', fontsize=14) #fontweight='bold', fontsize=16, family='Sans', labelpad=10)
+ax2.tick_params(axis='both', which='major', labelsize=12, length=6, width=1)
 
 fn = str(sys.argv[2])+"_normCov_GC.png"
-fig1.set_size_inches(5,4)
-plt.savefig(fn, dpi=600, bbox_inches='tight')
+plt.savefig(fn, size=(5,4), dpi=600)#, bbox_inches='tight')
+print("Done plotting!")
